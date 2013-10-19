dc897ce9664ffba3497e7cd72c2718694fba8612
Alessio Fattorini <alessio.fattorini@nethesis.it>
2013-10-19 09:30:10 +0000

    Removed smtp configuration and added language

--- root/var/www/html/owncloud/config/autoconfig.php
+++ root/var/www/html/owncloud/config/autoconfig.php
@@ -9,8 +9,6 @@ $AUTOCONFIG = array(
   "adminlogin"    => "admin",
   "adminpass"     => "Nethesis,1234",
   "directory"     => "/var/www/html/owncloud/data",
-  "mail_smtpmode"     => "smtp",
-  "mail_smtphost"     => "localhost",
-  "mail_smtpport"     => 25,
+  "default_language" => "it",
 );
 
